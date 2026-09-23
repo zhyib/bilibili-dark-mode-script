a622998e497221cd3bceec0a01119cda39489895
Yibin Z. <zhyib002@outlook.com>
2026-09-23 22:28:35 +0800

    update: license

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 // @match        https://*.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
-// @license      MIT
+// @license      CC-BY-NC-SA-4.0
 // ==/UserScript==
 
 (function () {
